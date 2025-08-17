be0f09d61b72df653d596665d0cb9a045409d116
somtam <tim@ocher.co>
2025-08-18 01:00:34 +0700

    Optional require stim in inventory

--- Plugin.cs
+++ Plugin.cs
@@ -6,7 +6,7 @@ using PlayerLives.Features;
 
 namespace PlayerLives
 {
-    [BepInPlugin("com.somtam.playerLives", "Player Lives", "1.1.5")]
+    [BepInPlugin("com.somtam.playerLives", "Player Lives", "1.2.0")]
     public class Plugin : BaseUnityPlugin
     {
         public static ManualLogSource LogSource;
