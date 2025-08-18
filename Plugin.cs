d04ff8b94ed60a18b331db423c058d2fa3ddf397
somtam <tim@ocher.co>
2025-08-18 21:23:57 +0700

    1.2.1

--- Plugin.cs
+++ Plugin.cs
@@ -6,7 +6,7 @@ using PlayerLives.Features;
 
 namespace PlayerLives
 {
-    [BepInPlugin("com.somtam.playerLives", "Player Lives", "1.2.0")]
+    [BepInPlugin("com.somtam.playerLives", "Player Lives", "1.2.1")]
     public class Plugin : BaseUnityPlugin
     {
         public static ManualLogSource LogSource;
